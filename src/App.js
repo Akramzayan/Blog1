0fc94ea02d44a7868f23431a6bb476b5326f7c0c
akram <akremzayen@gmail.com>
2024-01-24 23:23:28 +0100

    architecture Done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 
 import './App.css';
-import Footer from './componenets/Footer';
+
 import HomePage from './pages/home/HomePage';
-import Articles from './pages/home/container/Articles';
-import CTA from './pages/home/container/CTA';
+
 
 
 function App() {
   return (
     <div className="App font-opensans">
       <HomePage/>
-      <Articles/>
-      <CTA/>
-      <Footer/>
-     
     </div>
   );
 }
