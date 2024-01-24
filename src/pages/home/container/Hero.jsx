877fde512d9cb18b6958a857ba6f089f63dc3568
akram <akremzayen@gmail.com>
2024-01-24 01:00:39 +0100

    HeroSection Done

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
+import {FiSearch} from 'react-icons/fi'
 import { images } from '../../../constants'
+
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Read The Most intereresting articles</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus nam sapiente minus explicabo optio facilis odit possimus impedit veritatis dolores, ducimus eaque quibusdam mollitia rem porro eveniet nihil officia modi?</p>
-      </div>
-      <div>
-        <div>
-            <input type="text" />
+    <section className='container mx-auto flex flex-col px-5 py-5 lg:flex-row'>
+      <div className='mt-10 lg:w-1/2'>
+        <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-left lg:max-w-[540px] '>Read The most Interesting articles</h1>
+        <p className='text-dark-light mt-4 text-center md:text-xl lg:text-left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, minus!</p>
+        <div className='flex flex-col gap-y-2.5 mt-10 relative'>
+          <div className='relative'>
+            <FiSearch className='absolute left-3 top-1/2  -translate-y-1/2 w-6 h-6 text-[#959Ead]'/>
+            <input type="text" className='placeholder:font-bold font-semibold text-dark-soft placeholder:text-[#959EAD] rounded-lg pl-12 pd-3 w-full py-3 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] md:py-4'
+             placeholder='Search Article'/>
+          </div>
+          <button className='w-full bg-primary text-white font-semibold rounded-lg px-5 py-3 md:absolute md:right-2 md:top-1/2 md:-translate-y-1/2 md:w-fit md:py-2'>
+            Search
+          </button>
         </div>
-        <button>Search</button>
+        <div className='flex mt-4 flex-col lg:flex-row lg:items-start lg:flex-nowrap lg:gap-x-4 lg:mt-7'>
+          <span className='text-dark-light font-semibold italic mt-2 lg:mt-4'>Popular Tags :</span>
+          <ul className='flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3'>
+            <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold '>Design</li>
+            <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Experience</li>
+            <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Interfaces</li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <span>Popular Taggs:</span>
-        <ul>
-            <li>Design</li>
-            <li>User Experience</li>
-            <li>User Interfaces</li>
-        </ul>
+      <div className='hidden lg:block lg:1/2'>
+        <img 
+        className='w-full'
+        src={images.HeroImage} alt="users are reading articles" />
       </div>
-      <div>
-        <img src={images} alt="users are reading articles" />
-      </div>
+      
     </section>
   )
 }
