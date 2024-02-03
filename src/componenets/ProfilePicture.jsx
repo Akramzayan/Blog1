2a262abed57ae358c45287e51fb3d75a446906f3
akram <akremzayen@gmail.com>
2024-02-03 02:31:25 +0100

    uploadPic

--- src/componenets/ProfilePicture.jsx
+++ src/componenets/ProfilePicture.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { stables } from '../constants'
 import {HiOutlineCamera} from "react-icons/hi"
 
+import {createPortal} from 'react-dom'
+import CropEasy from './crop/cropEasy'
+
+
+
+
+
+
 const ProfilePicture = ({avatar}) => {
+  const [photo,setPhoto] = useState(null) 
+
+  const [openCrop,setOpenCrop] = useState(false)
+  const handleFileChange =(e) => {
+    const file = e.target.files[0];
+    setPhoto({url : URL.createObjectURL(file),file});
+    setOpenCrop(true)
+
+  }
   return (
+    <>
+    {openCrop && createPortal(<CropEasy photo={photo} setOpenCrop={setOpenCrop}/>,document.getElementById('portal')) } 
     <div className='w-full flex items-center gap-x-4'> 
       <div className='relative h-20 w-20 rounded-full outline outline-offset-2 outline-1 lutline-primary overflow-hidden '>
         <label htmlFor="profilePicture" className='cursor-pointer absolute inset-0 rounded-full bg-transparent'>
@@ -14,13 +33,15 @@ const ProfilePicture = ({avatar}) => {
                     <HiOutlineCamera className='w-7 h-auto text-primary'/>
                 </div>
                )}
-            <input type="file" className='sr-only' id='profilePicture'/>{/*sr-only == hidden*/}
+            <input type="file" className='sr-only' id='profilePicture' onChange={handleFileChange}/>{/*sr-only == hidden*/}
         </label>
       </div>
       <button type='button' className='border bored-red-500 rounded-lg px-4 py-2 text-red-500'>
         Delete
       </button>
     </div>
+    </>
+    
   )
 }
 
