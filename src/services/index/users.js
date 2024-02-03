9de8cfa36759fd79a6809c34f8951d25636f105e
akram <akremzayen@gmail.com>
2024-02-04 00:16:31 +0100

    Upload Funcionnality Tottaly Done

--- src/services/index/users.js
+++ src/services/index/users.js
@@ -31,11 +31,11 @@ export const login = async ({ email, password }) => {
 export const getUserProfile = async ({ token }) => {
   try {
     const config = {
-      headers:{
-        Authorization:`Bearer ${token}`
-      }
-    }
-    const { data } = await axios.get("/api/users/profile",config);
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data } = await axios.get("/api/users/profile", config);
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
@@ -43,14 +43,18 @@ export const getUserProfile = async ({ token }) => {
     throw new Error(error.message);
   }
 };
-export const updateProfile = async ({ token,userData}) => {
+export const updateProfile = async ({ token, userData }) => {
   try {
     const config = {
-      headers:{
-        Authorization:`Bearer ${token}`
-      }
-    }
-    const { data } = await axios.put("/api/users/updateProfile",userData,config);
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data } = await axios.put(
+      "/api/users/updateProfile",
+      userData,
+      config
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
@@ -59,15 +63,19 @@ export const updateProfile = async ({ token,userData}) => {
   }
 };
 
-export const updateProfilePicture = async ({ token,formData}) => {
+export const updateProfilePicture = async ({ token, formData }) => {
   try {
     const config = {
-      headers:{
-        "Content-Type":"multipart/form-data",
-        Authorization:`Bearer ${token}`
-      }
-    }
-    const { data } = await axios.put("/api/users/updateProfilePicture",formData,config);
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const { data } = await axios.put(
+      "/api/users/updateProfilePicture",
+      formData,
+      config
+    );
     return data;
   } catch (error) {
     if (error.response && error.response.data.message)
